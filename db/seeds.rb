4bbbc84091152999b141e7de5a344711da95f529
Alois Beard <alois.beard@gmail.com>
2019-02-20 15:47:53 +0100

    Dion told me to Push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Matsuri',
+    address:      '23 rue des cerisiers en fleur',
+    category:     'japanese'
+  },
+  {
+    name:         'Crazy Dim Sum',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'chinese'
+  },
+   {
+    name:         'Le saucisson',
+    address:      '5 rue de DTC',
+    category:     'french'
+  },
+   {
+    name:         'Pizza Hut',
+    address:      '33 avenue des pieds niques',
+    category:     'italian'
+  },
+   {
+    name:         'Frites',
+    address:      'avenue des frites grilles',
+    category:     'belgian'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
